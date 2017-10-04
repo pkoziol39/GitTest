69ae90fe89e6a122dbba26d16583f69a7605a9b3
Piotr Kozioł <piotr@Mac-mini-Piotr.local>
2017-10-04 10:31:49 +0200

    Add new map feature

---
+++ GitTest/MapForItinerary.swift
@@ -0,0 +1,13 @@
+//
+//  MapForItinerary.swift
+//  GitTest
+//
+//  Created by Piotr Kozioł on 04.10.2017.
+//  Copyright © 2017 Piotr Kozioł. All rights reserved.
+//
+
+import UIKit
+
+class MapForItinerary: NSObject {
+    
+}
